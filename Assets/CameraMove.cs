cf01a6dc8664c60331c120ea0f15e2595938bf58
Vladislav <ulqrbl@gmail.com>
2020-02-29 20:10:08 +0300

    Step 1

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        cam.transform.position += new Vector3(0, v, 0) * Time.deltaTime;
+    }
+}
